8e977f6267af475797c0bb0a9b203e52b88c8982
alex-reardon <arear001@ucr.edu>
2026-06-26 13:09:43 -0400

    stop tracking *

--- old/A_old/noise_removal.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def remove_trailing_noise(text):
-    text = re.sub(r'(?<=[a-z])\d{2,}$', '', text)
-    text = re.sub(r'[^\w\s/+.-]+', ' ', text)
-    text = re.sub(r'\.{2,}', ' ', text)
-    return text
